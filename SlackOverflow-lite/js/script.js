bc9a6c8e959b9f6bd302e59d005f5f2f46201046
Haruna Ibn Yusuf <har.mullah@gmail.com>
2018-08-22 15:52:05 +0300

    Origanise UI

---
+++ SlackOverflow-lite/js/script.js
@@ -0,0 +1,10 @@
+function remove1() {
+    var elem = document.getElementById('delete1');
+    elem.parentNode.removeChild(elem);
+    return false;
+}
+function remove2() {
+    var elem2 = document.getElementById('delete2');
+    elem.parentNode.removeChild(elem2);
+    return false;
+}
