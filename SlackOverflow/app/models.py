4214e5473ca57a4b57ec257c19c8844f3b573526
Haruna Ibn Yusuf <har.mullah@gmail.com>
2018-08-23 14:52:14 +0300

    Update api

---
+++ SlackOverflow/app/models.py
@@ -0,0 +1,42 @@
+"""
+    Data module or user flow
+
+"""
+
+class Blog():
+
+    counter = [1,]
+    container = []
+    questions = dict()
+    answer = dict()
+    incrementer = 1
+
+
+    def data_post(self,question):
+        """store post data"""
+        self.container.append(question)
+        return(self.container)
+
+    def id_generator(self):
+        """Question id generation"""
+
+        self.counter[0] += 3
+        return self.counter[0]
+
+    def post_answer(self): 
+        """capturing posted answer"""
+        pass
+
+    def capture_question(self,question):
+        """capturing question """
+        if question != "":
+            self.questions = {"id": self.id_generator(), "question": question}
+            #question post
+            self.data_post(self.questions)
+            return True
+        return False
+    
+
+
+
+
---
+++ SlackOverflow/app/views.py
@@ -0,0 +1,78 @@
+""""
+    display module
+"""
+
+from flask_restful import Resource
+
+from flask import request
+
+from app.models import Blog
+
+from flask import jsonify, make_response
+
+
+class Supporter:
+    """supports display module"""
+
+    def response(self, status):
+        json_conv = jsonify({"status": status})
+        return make_response(json_conv)
+
+
+class Query(Resource, Blog, Supporter):
+
+    def post(self):
+        """post question"""
+
+        if request.content_type == 'application/json':
+            message = request.get_json()
+            post = message["Note"]
+            if self.capture_question(post):
+                return self.response("Bravo! Your question is posted.")
+            return self.response("Enble to display question sorry!")
+        
+
+class Fetch(Resource,Blog, Supporter):
+    """ Display all questions"""
+
+    def get(self):
+        content = self.container
+        return self.response(content)
+
+
+class FetchSpecific(Resource, Blog, Supporter):
+    """ Display specific questions"""
+
+    def get(self, questionId):
+        try:
+            spec=int(questionId)
+        except ValueError:
+            return('Non-numeric data found in the file.')
+        else:
+            for string in self.container:
+                if string["id"] == spec:
+                    return self.response(string)
+            return("Sorry Id provide does not exist")
+
+
+class AnswerSpecific(Resource, Blog, Supporter):
+    """Post answer to specific question"""
+
+    def post(self, questionId):
+
+        try:
+            spec = int(questionId)
+        except ValueError:
+            return('Non-numeric data found in the file.')
+        else:
+            for string in self.container:
+                if string["id"] == spec:
+                    # "comment" = self.response(string)
+                    if request.content_type == 'application/json':
+                        comment = request.get_json()
+                        post = comment["Note"]
+                        string["comment"] = post
+                        return self.response("success")
+                    return self.response("not success")
+
+        return("Sorry Id provide does not exist")
